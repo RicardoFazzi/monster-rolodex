21681bef1bb8dd64a72469cae7728ef349ac7627
Ricardo Fazzi <ricardofazzi007@gmail.com>
2020-03-14 15:41:40 -0300

    Updates

---
+++ precache-manifest.ffa188e1da98527cb3a878a71bb5a762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc9db6b47d9818eab3af5105316e213",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "fab591443f3d20bf2d6b",
+    "url": "/monster-rolodex/static/css/main.1b9453f3.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monster-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab591443f3d20bf2d6b",
+    "url": "/monster-rolodex/static/js/main.75c22700.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
